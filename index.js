6ac0404195509ceeec6d99625be24ac17559e15c
Munieru <20086673+munierujp@users.noreply.github.com>
2022-10-07 22:42:42 +0900

    Use eslint-plugin-jsdoc

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: [
     'standard',
     'plugin:unicorn/recommended',
-    'plugin:eslint-comments/recommended'
+    'plugin:eslint-comments/recommended',
+    'plugin:jsdoc/recommended'
   ]
 }
