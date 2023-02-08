bce22cd0c02830e9fb68cd12f33f6e07bc2dbf6c
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-08 09:45:10 +0900

    Add plugin:sonarjs/recommended

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'standard',
     'plugin:unicorn/recommended',
+    'plugin:sonarjs/recommended',
     'plugin:eslint-comments/recommended',
     'plugin:jsdoc/recommended'
   ]
