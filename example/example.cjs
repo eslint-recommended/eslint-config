ce88dda19da578dfb6ffcd536b06f04e22ead56c
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-21 08:43:06 +0900

    Add examples for .mjs/.cjs (#230)

---
+++ example/example.cjs
@@ -0,0 +1,12 @@
+// Try ESLint on this file
+
+/**
+ * @param {string} userName - user name
+ * @returns {void}
+ */
+const sayHello = (userName) => {
+  console.log(`Hello, ${userName}`)
+}
+
+const myName = 'Alice'
+sayHello(myName)
